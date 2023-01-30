ff8d6018c9a1a7a060b83ef4343f9d9e6962ee13
Ace-Azile <llamb04@icloud.com>
2023-01-30 18:37:09 -0500

    found X&Y dist. totalDist. & targetAngle

--- src/main/java/frc/robot/drive/PhotonVision/PhotonVision.java
+++ src/main/java/frc/robot/drive/PhotonVision/PhotonVision.java
@@ -11,13 +11,10 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import pabeles.concurrency.ConcurrencyOps.NewInstance;
 
 
 
@@ -31,7 +28,7 @@ public class PhotonVision extends SubsystemBase {
 
   }
 
-  public boolean findTarget(){
+  private boolean findTarget(){
     // locates targets, returns true when target is found
     var result = camera.getLatestResult();
     boolean hasTargets = result.hasTargets();
@@ -55,7 +52,7 @@ public class PhotonVision extends SubsystemBase {
     return 0;
   }
 
-  public double getXDistanceToTarget(){
+  private double getXDistanceToTarget(){
     // gets foward distance in meters
     var result = camera.getLatestResult();
 
@@ -63,14 +60,8 @@ public class PhotonVision extends SubsystemBase {
      
       PhotonTrackedTarget target = result.getBestTarget();
       Transform3d bestCameraToTarget = target.getBestCameraToTarget();
-      Translation3d cameraOffset = new Translation3d(-0.2667, 0, 0);
-      Rotation3d cameraRotationOffset = new Rotation3d(0, 0, Math.PI);
 
-      Transform3d cameraToRobot = new Transform3d(cameraOffset, cameraRotationOffset);
-
-      Transform3d targetToRobot = cameraToRobot.plus(bestCameraToTarget.inverse());
-
-      return targetToRobot.getX();
+      return bestCameraToTarget.getX();
 
     }
 
@@ -79,7 +70,7 @@ public class PhotonVision extends SubsystemBase {
   }
 
 
-  public double getYDistanceToTarget(){
+  private double getYDistanceToTarget(){
     // gets left/right distance in meters (left is positive)
     var result = camera.getLatestResult();
 
@@ -87,92 +78,27 @@ public class PhotonVision extends SubsystemBase {
      
       PhotonTrackedTarget target = result.getBestTarget();
       Transform3d bestCameraToTarget = target.getBestCameraToTarget();
-      Translation3d cameraOffset = new Translation3d(-0.2667, 0, 0);
-      Rotation3d cameraRotationOffset = new Rotation3d(0, 0, Math.PI);
 
-      Transform3d cameraToRobot = new Transform3d(cameraOffset, cameraRotationOffset);
+      return bestCameraToTarget.getY();
 
-      Transform3d targetToRobot = cameraToRobot.plus(bestCameraToTarget.inverse());
-      
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-package frc.robot.drive.PhotonVision;
-
-import java.util.List;
-
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonUtils;
-import org.photonvision.targeting.PhotonTrackedTarget;
-
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-
-
-public class PhotonVision extends SubsystemBase {
-  /** Creates a new PhotonVision. */
-  PhotonCamera camera;
-  
-
-  public PhotonVision() {
-    
-    camera = new PhotonCamera("Microsoft_LifeCam_HD-3000");
-
-  }
-
-  private boolean findTarget(){
-    var result = camera.getLatestResult();
-    boolean hasTargets = result.hasTargets();
-
-    return hasTargets;
-
-  }
-
-  private int getTagID(){
-    var result = camera.getLatestResult();
-  
-    if (findTarget()){
-      //List<PhotonTrackedTarget> targets = result.getTargets();
-      PhotonTrackedTarget target = result.getBestTarget();
-
-      int targetID = target.getFiducialId();
-
-      return targetID;
-    }
-    return 0;
-  }
-
-  private double getDistanceToTarget(){
-    var result = camera.getLatestResult();
-
-    //Camera and Target Positions
-    double cameraHeightMeters = Units.inchesToMeters(33 + (7/32));
-    double targetHeightMeters = Units.inchesToMeters(39 + (3/16));
-    double cameraPitchRadians = .108;
-
-
-
-    if (findTarget()){
-      double range = PhotonUtils.calculateDistanceToTargetMeters(
-                                                  cameraHeightMeters,
-                                                  targetHeightMeters,
-                                                  cameraPitchRadians,
-                                                  Units.degreesToRadians(result.getBestTarget().getPitch()));
-
-      //System.out.println(Units.degreesToRadians(result.getBestTarget().getPitch()));
-
-      return range;
     }
 
     return 0;
+
+  }
+
+
+  private double getTargetAngle(double x, double y){
+
+    // gives angle of the target in degrees
+    double targetAngle = (Math.tan(y/x)) * (180/Math.PI);
+    return targetAngle;
 
   }
 
 
   private double getPitch(){
+    // gets pitch of target in radians
     var result = camera.getLatestResult();
 
     if (findTarget()){
@@ -186,21 +112,29 @@ public class PhotonVision extends SubsystemBase {
     return 0;
   }
 
+  private double distanceFormula(double x, double y){
+    // returns distance to target in meters
+    return Math.sqrt((x*x)+(y*y));
+  }
+
 
 
   public void updateSmartDash(){
 
     SmartDashboard.putBoolean("hasTargets",findTarget());
-    SmartDashboard.putNumber("Target ID", getTagID());
-    SmartDashboard.putNumber("Pitch (Deg)", getPitch());
-    SmartDashboard.putNumber("Target Distance", getDistanceToTarget());
-   
+    //SmartDashboard.putNumber("Target ID", getTagID());
+    //SmartDashboard.putNumber("Pitch (Deg)", getPitch());
+    //SmartDashboard.putNumber("Target X Distance", getXDistanceToTarget());
+    //SmartDashboard.putNumber("Target Y Distance", getYDistanceToTarget());
+    SmartDashboard.putNumber("Target Total Distance", distanceFormula(getXDistanceToTarget(), getYDistanceToTarget()));
+    SmartDashboard.putNumber("Target Angle", getTargetAngle(getXDistanceToTarget(), getYDistanceToTarget()));
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+<<<<<<< HEAD
 }
     
 
@@ -259,4 +193,6 @@ public class PhotonVision extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
+=======
+>>>>>>> 643ca6f (found X&Y dist. totalDist. & targetAngle)
 }
