f3ad20729cf858152260340f41f11c1d03c88696
Logan Cone <loganscottcone@gmail.com>
2023-02-06 20:17:37 -0500

    added PID

--- src/main/java/frc/robot/auto/commands/AutoCommand.java
+++ src/main/java/frc/robot/auto/commands/AutoCommand.java
@@ -2,10 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.drive.commands;
+package frc.robot.auto.commands;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.drive.SwerveDrive;
 import frc.robot.drive.PhotonVision.PhotonVision;
@@ -21,6 +22,8 @@ public class AutoCommand extends CommandBase {
   /** Creates a new AutoTest. */
   public AutoCommand(SwerveDrive swerve) {
     this.swerve = swerve;
+
+    
     addRequirements(swerve);
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -93,10 +96,12 @@ public class AutoCommand extends CommandBase {
     driveToTarget(0.2, 2);
     //driveForSeconds(speeds, 3);
 
-    //driveForSeconds(speeds, 3);
-    driveForMeters(speeds, 1);
-
-    System.out.println(pid.calculate(swerve.getDistanceMeters(), 1));
+    SmartDashboard.putBoolean("toleranceStop", pid.atSetpoint());
+    if(pid.atSetpoint() == false){
+      driveForMeters(speeds, 1);
+    }else{
+      end(isFinished());
+    }
   }
 
   // Called once the command ends or is interrupted.
