1dda916ee3b19bc63589a5569d1f663be40539bf
Logan Cone <loganscottcone@gmail.com>
2023-02-06 20:17:37 -0500

    added PID

--- src/main/java/frc/robot/auto/commands/AutoCommand.java
+++ src/main/java/frc/robot/auto/commands/AutoCommand.java
@@ -2,12 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.drive.commands;
+package frc.robot.auto.commands;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.drive.SwerveDrive;
+import edu.wpi.first.math.controller.PIDController;
 
 
 public class AutoCommand extends CommandBase {
@@ -19,6 +21,8 @@ public class AutoCommand extends CommandBase {
   /** Creates a new AutoTest. */
   public AutoCommand(SwerveDrive swerve) {
     this.swerve = swerve;
+
+    
     addRequirements(swerve);
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -45,6 +49,9 @@ public class AutoCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    pid = new PIDController(0.5, 0, 0);
+    pid.setTolerance(0.01);
+
     timer = new Timer();
     timer.reset();
     timer.start();
@@ -53,11 +60,15 @@ public class AutoCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    ChassisSpeeds speeds = new ChassisSpeeds(0.3, 0, 0);
-    driveForSeconds(speeds, 3);
+    //System.out.println("PID" + pid.calculate(swerve.getDistanceMeters(), 1));
+    ChassisSpeeds speeds = new ChassisSpeeds(pid.calculate(swerve.getDistanceMeters(), 1), 0, 0);
 
-    //driveForMeters(0.3, 1.0);
-      
+    SmartDashboard.putBoolean("toleranceStop", pid.atSetpoint());
+    if(pid.atSetpoint() == false){
+      driveForMeters(speeds, 1);
+    }else{
+      end(isFinished());
+    }
   }
 
   // Called once the command ends or is interrupted.
