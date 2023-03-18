70942304daaa0c66e57c1013770dd41d05ffeae4
Michelle Guo <runbo.guo.17@gmail.com>
2023-03-17 22:20:56 -0400

    adjused auto mid score balance to go a bit further

--- src/main/java/frc/robot/auto/SequentialCommands/AutoScoreMidBalance.java
+++ src/main/java/frc/robot/auto/SequentialCommands/AutoScoreMidBalance.java
@@ -6,6 +6,7 @@ package frc.robot.auto.SequentialCommands;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -16,6 +17,7 @@ import frc.robot.armoutreach.commands.StowInside;
 import frc.robot.auto.commands.AutoDriveForDistance;
 import frc.robot.auto.commands.AutoDriveWithVelocity;
 import frc.robot.claw.Claw;
+import frc.robot.claw.commands.CloseClaw;
 import frc.robot.claw.commands.OpenClaw;
 import frc.robot.drive.SwerveDrive;
 import frc.robot.intake.Intake;
@@ -27,11 +29,15 @@ public class AutoScoreMidBalance extends SequentialCommandGroup {
   /** Creates a new AutoScoreMidBalance. */
   public AutoScoreMidBalance(SwerveDrive swerve, Intake intake, ArmOutreach arm, Claw claw) {
     swerve.resetHeading();
-    addCommands(new GoToMid(arm),
+    addCommands(
+                new CloseClaw(claw),
+                new GoToMid(arm),
                 new AutoDriveForDistance(swerve, 0.75, 0, new Rotation2d()),
                 new OpenClaw(claw),
                 new StowInside(arm),
-                new AutoDriveWithVelocity(swerve, -2.52, 0, new Rotation2d(), 0.7)
+                new AutoDriveWithVelocity(swerve, -2.65, 0, new Rotation2d(), 0.7),
+                new AutoDriveWithVelocity(swerve, 0, 0, Rotation2d.fromDegrees(180), 0.7),
+                new InstantCommand(swerve::resetHeading)
                 );
   }
 }
